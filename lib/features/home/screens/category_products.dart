949b6e52d6675bd832406a6956dc9785e3bd225b
Arun Kumar Bidila <arunkumarbidila@gmail.com>
2026-01-12 17:25:51 +0530

    created splash

--- lib/features/home/screens/category_products.dart
+++ lib/features/home/screens/category_products.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shoflex/core/constants/app_colors.dart';
 import 'package:shoflex/core/utils/app_data.dart';
+import 'package:shoflex/features/product/screens/product_detail_screen.dart';
 
 class CategoryProducts extends StatelessWidget {
   final String categoryName;
@@ -71,55 +72,63 @@ class CategoryProducts extends StatelessWidget {
                 ),
                 itemBuilder: (context, index) {
                   final product = filteredProducts[index];
-                  return Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        color: AppColors.fillColor),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Expanded(
-                          child: Stack(
-                            children: [
-                              Center(
-                                child: Image.asset(product.images[0],
-                                    fit: BoxFit.contain),
-                              ),
-                              Positioned(
-                                top: 10,
-                                right: 10,
-                                child: SvgPicture.asset(
-                                  "assets/like.svg",
-                                  width: 15,
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(
+                          context, ProductDetailScreen.routeName,
+                          arguments: product);
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: AppColors.fillColor),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            child: Stack(
+                              children: [
+                                Center(
+                                  child: Image.asset(product.images[0],
+                                      fit: BoxFit.contain),
                                 ),
-                              ),
-                            ],
+                                Positioned(
+                                  top: 10,
+                                  right: 10,
+                                  child: SvgPicture.asset(
+                                    "assets/like.svg",
+                                    width: 15,
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(5),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                product.name,
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.w500, fontSize: 12),
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                              const SizedBox(height: 4),
-                              Text(
-                                "₹${product.price.toStringAsFixed(2)}",
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.w700,
-                                    fontSize: 12,
-                                    fontFamily: "Gabarito"),
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
+                          Padding(
+                            padding: EdgeInsets.all(5),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  product.name,
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 12),
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                const SizedBox(height: 4),
+                                Text(
+                                  "₹${product.price.toStringAsFixed(2)}",
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 12,
+                                      fontFamily: "Gabarito"),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   );
                 },
